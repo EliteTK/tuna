84a3b220943803bf8e111c5582ff2849555d1abb
Tomasz Kramkowski <tk@the-tk.com>
2015-03-24 23:10:45 +0000

    Removed unneeded includes.

--- kernel.c
+++ kernel.c
@@ -1,10 +1,7 @@
 #include "term.h"
-#include <stdbool.h>
-#include <stddef.h>
-#include <stdint.h>
 
 void kernel_main()
 {
 	term_init();
-	term_write("This is a very interesting test of printing something and damn it, it's interesting!");
+	term_write("Test.\n");
 }
